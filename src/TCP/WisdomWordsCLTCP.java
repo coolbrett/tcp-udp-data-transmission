0129d667510d253307ceac252e9f15e931a85245
Brett Dale <brettdale828@gmail.com>
2021-09-22 00:09:28 -0400

    trying out TCP

--- src/TCP/WisdomWordsCLTCP.java
+++ src/TCP/WisdomWordsCLTCP.java
@@ -1,4 +1,42 @@
 package TCP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Random;
+
+
 public class WisdomWordsCLTCP {
+
+    public static void main(String[] args){
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        System.out.println("Connecting to " + host + " on port " + port);
+
+        try{
+            Socket socket = new Socket(InetAddress.getByName(host), port);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            int size = Integer.parseInt(bufferedReader.readLine());
+            System.out.println("Client received size: " + size);
+
+            int random = new Random().nextInt(size + 1);
+            System.out.println("Client's random number: " + random);
+            printWriter.println(random);
+
+            String saying = bufferedReader.readLine();
+            System.out.println("Client received saying: " + saying);
+
+            socket.close();
+            printWriter.close();
+            bufferedReader.close();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
--- src/TCP/WisdomWordsServTCP.java
+++ src/TCP/WisdomWordsServTCP.java
@@ -1,4 +1,45 @@
 package TCP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class WisdomWordsServTCP {
+
+    public static void main(String[] args){
+        try {
+            WisdomWords wisdomWords = new WisdomWords(args[0]);
+            int size = wisdomWords.getSize();
+
+            ServerSocket serverSocket = new ServerSocket(5000);
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                printWriter.println(size);
+                int index = Integer.parseInt(bufferedReader.readLine());
+                if (index < size) {
+                    System.out.println("Server index received: " + index);
+                    String saying = wisdomWords.getWisdom(index);
+                    System.out.println("Server sending saying: " + saying);
+                    printWriter.println(saying);
+                }else {
+                    System.out.println("Server: Index out of bounds");
+                }
+                socket.close();
+                inputStreamReader.close();
+                bufferedReader.close();
+                printWriter.close();
+            }
+            serverSocket.close();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
 }
